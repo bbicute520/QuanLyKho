4c28a3b9179386a987e35b08063e40b4ffb669e9
doan-thai <doanngocthai25102005.nd@gmail.com>
2026-03-28 22:46:50 +0700

    feat : khoi tao DbContext va API CRUD Product

---
+++ Inventory.Service/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Inventory.Service.Data;
+using Inventory.Service.Models;
+
+[Route("api/inventory/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly WarehouseDbContext _context;
+
+    public ProductController(WarehouseDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/inventory/product
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    // Bạn sẽ viết tiếp các method [HttpPost], [HttpPut], [HttpDelete] ở đây
+}
